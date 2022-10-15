4101d2b54b44b5e755dfdd1c3344b1425dfd1595
Ellie <octopluma@gmail.com>
2022-10-15 13:29:08 +0200

    chore(universal): add basic file structure

---
+++ halloween.js
@@ -0,0 +1,20 @@
+// Make the timer function update every second
+var myfunc = setInterval(function() {
+    // var that sets the end date
+    var countDownDate = new Date("Oct 31, 2022 00:00:00").getTime();
+    }, 1000)
+
+// Calculate the remaining time in days, hours, minutes, and seconds
+var now = new Date().getTime();
+var timeleft = countDownDate - now;
+    
+var days = Math.floor(timeleft / (1000 * 60 * 60 * 24));
+var hours = Math.floor((timeleft % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+var minutes = Math.floor((timeleft % (1000 * 60 * 60)) / (1000 * 60));
+var seconds = Math.floor((timeleft % (1000 * 60)) / 1000);
+
+// Display the output to users
+document.getElementById("days").innerHTML = days + "d "
+document.getElementById("hours").innerHTML = hours + "h " 
+document.getElementById("mins").innerHTML = minutes + "m " 
+document.getElementById("secs").innerHTML = seconds + "s"
