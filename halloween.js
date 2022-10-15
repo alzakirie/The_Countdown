de95a7e4e56b5cbbe20d196d913c6f18581afb82
Ellie <octopluma@gmail.com>
2022-10-15 13:47:17 +0200

    feat(Countdown): add basic JavaScript Countdown

--- halloween.js
+++ halloween.js
@@ -1,20 +1,28 @@
-// Make the timer function update every second
-var myfunc = setInterval(function() {
-    // var that sets the end date
-    var countDownDate = new Date("Oct 31, 2022 00:00:00").getTime();
-    }, 1000)
+// Set the date we're counting down to
+var countDownDate = new Date("Oct 31, 2022 00:00:00").getTime();
 
-// Calculate the remaining time in days, hours, minutes, and seconds
-var now = new Date().getTime();
-var timeleft = countDownDate - now;
-    
-var days = Math.floor(timeleft / (1000 * 60 * 60 * 24));
-var hours = Math.floor((timeleft % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
-var minutes = Math.floor((timeleft % (1000 * 60 * 60)) / (1000 * 60));
-var seconds = Math.floor((timeleft % (1000 * 60)) / 1000);
+// Update the count down every 1 second
+var x = setInterval(function() {
 
-// Display the output to users
-document.getElementById("days").innerHTML = days + "d "
-document.getElementById("hours").innerHTML = hours + "h " 
-document.getElementById("mins").innerHTML = minutes + "m " 
-document.getElementById("secs").innerHTML = seconds + "s"
+  // Get today's date and time
+  var now = new Date().getTime();
+
+  // Find the distance between now and the count down date
+  var distance = countDownDate - now;
+
+  // Time calculations for days, hours, minutes and seconds
+  var days = Math.floor(distance / (1000 * 60 * 60 * 24));
+  var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+  var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+  var seconds = Math.floor((distance % (1000 * 60)) / 1000);
+
+  // Display the result in the element with id="demo"
+  document.getElementById("demo").innerHTML = days + "d " + hours + "h "
+  + minutes + "m " + seconds + "s ";
+
+  // If the count down is finished, write some text
+  if (distance < 0) {
+    clearInterval(x);
+    document.getElementById("demo").innerHTML = "EXPIRED";
+  }
+}, 1000);
